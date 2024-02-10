9cd2665362e2b0e7d5ee83d560197ae0f5214459
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2024-02-10 20:34:45 -0300

    feat: implementing GetCurrentAuctionUseCase

---
+++ src/AuctionWebAPI/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using AuctionWebAPI.Entities;
+using AuctionWebAPI.Repositories;
+
+namespace AuctionWebAPI.UseCases.Auctions.GetCurrent
+{
+    public class GetCurrentAuctionUseCase
+    {
+        public Auction? Execute()
+        {
+            var repository = new AuctionWebAPIDbContext();
+            var today = DateTime.UtcNow;
+            
+            return repository
+                .Auctions
+                .Include(auction => auction.Items)
+                .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
+        }
+    }
+}
