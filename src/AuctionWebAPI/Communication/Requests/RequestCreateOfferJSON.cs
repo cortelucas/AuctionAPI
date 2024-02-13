2eefac81f953b89fa0502b05e48f4b4f7e0568a2
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2024-02-12 21:31:56 -0300

    feat: implementing body request for new offer

---
+++ src/AuctionWebAPI/Communication/Requests/RequestCreateOfferJSON.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionWebAPI.Communication.Requests
+{
+    public class RequestCreateOfferJSON
+    {
+        public decimal Price { get; set; }
+    }
+}
